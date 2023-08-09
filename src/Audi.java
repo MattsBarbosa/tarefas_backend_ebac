8984df2f85b378f0eb269e1a04a05f15d1a3ecd0
MattsBarbosa <matts.barbosa@gmail.com>
2023-08-09 13:22:43 -0300

    Removendo arquivos

--- src/Audi.java
+++
@@ -1,7 +0,0 @@
-public class Audi extends Carros{
-    @Override
-    void printaModeloDoCarro() {
-        System.out.println("Modelo Audi V8 2023");
-    }
-
-}
--- src/Toyota.java
+++
@@ -1,7 +0,0 @@
-public class Toyota extends Carros{
-
-    @Override
-    void printaModeloDoCarro() {
-        System.out.println("Modelo Toyota 2022");
-    }
-}
