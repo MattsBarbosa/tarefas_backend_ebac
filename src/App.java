c91f30fd56bbb94cf57b05b7ba1773e81a22f192
MattsBarbosa <matts.barbosa@gmail.com>
2023-09-12 10:57:47 -0300

    tarefa concluida

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class App {
+
+    public static void main (String[] args) {
+        List<Pessoa> listaPessoas = Pessoa.popularPessoas().stream()
+                .filter(pessoa -> pessoa.getSexo().equals("Feminino"))
+                .toList();
+
+        System.out.println(listaPessoas);
+    }
+}
